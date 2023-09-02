0fc461679d74a26b28e598f421be7d9ffa44fb17
Nicola Fiori (JD342) <nicola@jd342.net>
2023-09-02 16:53:59 +0100

    Get it to show a window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,69 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "SDL.h"
+#include "main.h"
 
-int main() {
+#define WINDOW_WIDTH  1280
+#define WINDOW_HEIGHT 720
+
+SDL_Window   *window;
+SDL_Renderer *renderer;
+
+int main()
+{
     printf("Wow this is crazy.");
-    return 0;   
+
+    init();
+
+    atexit(cleanup);
+
+    SDL_Event event;
+
+    while (1)
+    {
+        SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
+    	SDL_RenderClear(renderer);
+
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT) break;
+        }
+
+        update();
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
+    }
+
+    return 0;
+}
+
+void init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    int w = WINDOW_WIDTH;
+    int h = WINDOW_HEIGHT;
+    int flags = 0;
+
+    if (SDL_CreateWindowAndRenderer(w, h, flags, &window, &renderer))
+    {
+        printf("Failed to open window: %s\n", SDL_GetError());
+        exit(1);
+    }
+}
+
+void update()
+{
+
+}
+
+void cleanup()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
---
+++ src/main.h
@@ -0,0 +1,7 @@
+int main();
+
+void init();
+
+void update();
+
+void cleanup();
