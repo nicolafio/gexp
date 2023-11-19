2c984ec55905628e542d93310e80e7a306346f19
Nicola Fiori (JD342) <nicola@jd342.net>
2023-11-19 17:50:19 +0000

    Automatic compile and restart on source change

---
+++ live-reload.sh
@@ -0,0 +1,46 @@
+WAIT_TIME_SECONDS=0.1
+SOURCES_DIR="src"
+CLIENT_EXECUTABLE="build/gexp"
+CLIENT_PROCESS=""
+
+function hash_sources() {
+  local hash=""
+  for f in $(find $SOURCES_DIR -type f)
+  do
+    hash+=$(cat $f)
+    hash=$(echo $hash | md5 -q)
+  done
+  echo $hash
+}
+
+SOURCES_HASH=$(hash_sources)
+
+function wait_for_change() {
+  echo waiting for change
+  local hash=""
+  while true
+  do
+    sleep $WAIT_TIME_SECONDS
+    hash=$(hash_sources)
+    if [ "$hash" != "$SOURCES_HASH" ]
+    then
+      SOURCES_HASH=$hash
+      break
+    fi
+  done
+}
+
+while true
+do 
+  make client || {
+    wait_for_change
+    continue
+  }
+  
+  $CLIENT_EXECUTABLE &
+  CLIENT_PROCESS=$!
+
+  wait_for_change
+
+  kill $CLIENT_PROCESS
+done
